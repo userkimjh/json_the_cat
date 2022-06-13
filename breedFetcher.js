f52737758344813c4be72be7bf1985a8335b7995
John Kim <96427720+userkimjh@users.noreply.github.com>
2022-06-13 14:44:37 -0400

    async return

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,15 +3,19 @@ const argv = process.argv;
 
 const breed = argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response ,body) => {
-  if (error) {
-    console.log('Page not found')
-  } else {
-    const data = JSON.parse(body); 
-    if (!data.length) {
-      console.log("Breed not found!")
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback('Error fetch details:', null);
     } else {
-      console.log(data[0].description)
-    }
-  }
-})
+      const data = JSON.parse(body); 
+      if (!data.length) {
+        callback("Breed not found!", null);
+      } else {
+        callback(null,data[0].description);
+      }
+    } 
+  })
+};
+
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,11 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    return error
+  } else {
+    return desc
+  }
+});
